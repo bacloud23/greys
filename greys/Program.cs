9838ecf70eec23bf3a70f62a56fbc132f20b4499
Yannayar <yanna92yar@yahoo.com>
2023-08-14 17:59:22 +0200

    * better cli

--- greys/Program.cs
+++ greys/Program.cs
@@ -1,6 +1,4 @@
 ﻿using greys;
-using System.Collections;
-using System.Collections.Generic;
 using System.Runtime.InteropServices;
 
 namespace ManagedColorPlayground
@@ -12,6 +10,20 @@ namespace ManagedColorPlayground
 
         static void Main(string[] args)
         {
+
+            List<string> colorFiltersStr = new List<string> {
+                "Neutral",
+                "Negative",
+                "GrayScale",
+                "NegativeGrayScale",
+                "Red",
+                "NegativeRed",
+                "Sepia",
+                "NegativeSepia",
+                "HueShift180",
+                "NegativeHueShift180"
+             };
+
             float[] identity = BuiltinMatrices.Identity.Cast<float>().ToArray();
             float[] Negative = BuiltinMatrices.Negative.Cast<float>().ToArray();
             float[] GrayScale = BuiltinMatrices.GrayScale.Cast<float>().ToArray();
@@ -23,20 +35,65 @@ namespace ManagedColorPlayground
             float[] HueShift180 = BuiltinMatrices.HueShift180.Cast<float>().ToArray();
             float[] NegativeHueShift180 = BuiltinMatrices.NegativeHueShift180.Cast<float>().ToArray();
 
-            List<string> colorFilters = new List<string> {
-                "Neutral",
-                "Negative",
-                "GrayScale",
-                "NegativeGrayScale",
-                "Red",
-                "NegativeRed",
-                "Sepia",
-                "NegativeSepia",
-                "HueShift180",
-                "NegativeHueShift180"
-              };
-            colorFilters = colorFilters.ConvertAll(d => d.ToLower());
-            Dictionary<string, string> descriptions = new Dictionary<string, string>();
+
+            List<float[]> colorFilters = new List<float[]> { identity, Negative, GrayScale, NegativeGrayScale, Red, NegativeRed, Sepia, NegativeSepia, HueShift180, NegativeHueShift180 };
+            var magEffectInvert = new MAGCOLOREFFECT
+            {
+                transform = Negative
+            };
+            MagInitialize();
+
+            int optionsCount = colorFiltersStr.Count;
+
+            int selected = 0;
+
+            bool done = false;
+
+            while (!done)
+            {
+                for (int i = 0; i < optionsCount; i++)
+                {
+                    if (selected == i)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.Write("> ");
+                    }
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+
+                    Console.WriteLine($"{i}> {colorFiltersStr[i]}");
+
+                    Console.ResetColor();
+                }
+
+                switch (Console.ReadKey(true).Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        selected = Math.Max(0, selected - 1);
+                        break;
+                    case ConsoleKey.DownArrow:
+                        selected = Math.Min(optionsCount - 1, selected + 1);
+                        break;
+                    case ConsoleKey.Enter:
+                        magEffectInvert = new MAGCOLOREFFECT
+                        {
+                            transform = colorFilters[selected]
+                        };
+                        MagSetFullscreenColorEffect(ref magEffectInvert);
+                        break;
+                }
+
+                if (!done)
+                    Console.CursorTop = Console.CursorTop - optionsCount;
+            }
+
+
+            Console.WriteLine($"Selected {selected}.");
+
+            colorFiltersStr = colorFiltersStr.ConvertAll(d => d.ToLower());
+            /*Dictionary<string, string> descriptions = new Dictionary<string, string>();
             descriptions.Add(colorFilters[0], "no color transformation");
             descriptions.Add(colorFilters[1], "simple colors transformation.");
             descriptions.Add(colorFilters[2], "simple colors transformation.");
@@ -46,112 +103,14 @@ namespace ManagedColorPlayground
             descriptions.Add(colorFilters[6], "simple colors transformation.");
             descriptions.Add(colorFilters[7], "simple colors transformation.");
             descriptions.Add(colorFilters[8], "simple colors transformation.");
-            descriptions.Add(colorFilters[9], "simple colors transformation.");
+            descriptions.Add(colorFilters[9], "simple colors transformation.");*/
 
-            foreach (KeyValuePair<string, string> kvp in descriptions)
+            /*foreach (KeyValuePair<string, string> kvp in descriptions)
             {
                 Console.WriteLine("Color filter = {0}, description = {1}", kvp.Key, kvp.Value);
-            }
-            Console.WriteLine("");
-            Console.WriteLine("Select a color filter to see elements on the screen better. Press X to exit: ");
+            }*/
 
-
-
-            string callSign;
-            string exitKey = "x";
-            var magEffectInvert = new MAGCOLOREFFECT
-            {
-                transform = Negative
-            };
-            while ((callSign = Console.ReadLine().ToLower()) != exitKey)
-            {
-                if (colorFilters.Contains(callSign))
-                {
-                    Console.WriteLine($"\"{callSign}\" Color filter set correctly");
-                    MagInitialize();
-                    switch (callSign)
-                    {
-                        case "neutral":
-                            MagUninitialize();
-                            break;
-                        case "negative":
-                            magEffectInvert = new MAGCOLOREFFECT
-                            {
-                                transform = Negative
-                            };
-                            MagSetFullscreenColorEffect(ref magEffectInvert);
-                            break;
-                        case "grayscale":
-                            magEffectInvert = new MAGCOLOREFFECT
-                            {
-                                transform = GrayScale
-                            };
-                            MagSetFullscreenColorEffect(ref magEffectInvert);
-                            break;
-                        case "negativegrayscale":
-                            magEffectInvert = new MAGCOLOREFFECT
-                            {
-                                transform = NegativeGrayScale
-                            };
-                            MagSetFullscreenColorEffect(ref magEffectInvert);
-                            break;
-                        case "red":
-                            magEffectInvert = new MAGCOLOREFFECT
-                            {
-                                transform = Red
-                            };
-                            MagSetFullscreenColorEffect(ref magEffectInvert);
-                            break;
-                        case "negativered":
-                            magEffectInvert = new MAGCOLOREFFECT
-                            {
-                                transform = NegativeRed
-                            };
-                            MagSetFullscreenColorEffect(ref magEffectInvert);
-                            break;
-                        case "sepia":
-                            magEffectInvert = new MAGCOLOREFFECT
-                            {
-                                transform = Sepia
-                            };
-                            MagSetFullscreenColorEffect(ref magEffectInvert);
-                            break;
-                        case "negativesepia":
-                            magEffectInvert = new MAGCOLOREFFECT
-                            {
-                                transform = NegativeSepia
-                            };
-                            MagSetFullscreenColorEffect(ref magEffectInvert);
-                            break;
-                        case "hueshift180":
-                            magEffectInvert = new MAGCOLOREFFECT
-                            {
-                                transform = HueShift180
-                            };
-                            MagSetFullscreenColorEffect(ref magEffectInvert);
-                            break;
-                        case "negativehueshift180":
-                            magEffectInvert = new MAGCOLOREFFECT
-                            {
-                                transform = NegativeHueShift180
-                            };
-                            MagSetFullscreenColorEffect(ref magEffectInvert);
-                            break;
-                        default:
-                            // code block
-                            break;
-                    }
-
-                    Console.WriteLine("");
-                }
-                else
-                {
-                    Console.WriteLine($"\"{callSign}\" color filter does not exist in our list");
-                }
-                Console.WriteLine("");
-                Console.WriteLine("Select a color filter to see elements on the screen better. Press X to exit: ");
-            }
-
+            
             Console.ReadLine();
             MagUninitialize();
 
